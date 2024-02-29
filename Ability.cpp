3efaebca31cabc109092e66320f3de08e2caa53d
66011582 <66011582@kmitl.ac.th>
2024-02-29 23:03:00 +0700

    เขียนตัวอย่าง ability, aff, stat ได้เขียนกันไฟลุกแน่

--- Ability.cpp
+++ Ability.cpp
@@ -1,4 +1,43 @@
 #include "Ability.h"
+#include "Affliction.h"
+#include "FundamentalSystem.cpp"
+#include "FundamentalSystem.h"
+
+#include "StatParam.h"
+
 using namespace std;
 
-// Fah
+void Eat::produceAffliction() { return; }
+
+void Eat::createStatParam() {
+  StatParam *aff = new Hunger(getParent()->getParent());
+}
+
+vector<GameObject *> Eat::findTargetForPassive() {
+  vector<GameObject *> target = {getParent()->getParent()};
+  return target;
+}
+
+vector<GameObject *> Eat::findTargetForActive() {
+  vector<GameObject *> target = {};
+  Layer *foodLayer =
+      getParent()->getParent()->getParent()->getParent()->getLayer("Food");
+  // ฟังก์ชั่นหา object ที่อยู่ห่างจากตัวเองไม่เกิน 1 ช่อง
+  return target;
+}
+
+bool Eat::canActive(vector<GameObject *> target) {
+  // ตรวจสอบว่ารอบตัวใน Layer Food มีอาหารอยู่รอบตัวไหม
+  return true;
+}
+
+void Eat::passive(vector<GameObject *> target) {
+  Hungry *hungry = new Hungry(getParent()->getParent());
+  return;
+}
+void Eat::active(vector<GameObject *> target) {
+  // ลบอาหารที่กินไป
+  // สร้าง Affliction food ที่จะบวก hunger ให้กับตัวเอง
+  Full *full = new Full(getParent()->getParent());
+  return;
+}
---
+++ Affliction.h
@@ -0,0 +1,37 @@
+#include "FundamentalSystem.h"
+
+using namespace std;
+
+#ifndef ABILITY_CPP
+#define ABILITY_CPP
+
+class Full : public Affliction {
+    protected:
+    string name = "Food";
+    string targetValue = "Hunger";
+    int duration = 5;
+    int valueIncrease = 0;
+    int value = 20;
+    bool permanent = false;
+    public:
+    Full(StatusBlock *parent);
+    Full(GameObject *target);
+};
+
+class Hungry : public Affliction {
+    protected:
+    string name = "Hungry";
+    string targetValue = "Hunger";
+    int duration = 21;
+    int valueIncrease = 0;
+    int value = -5;
+    bool permanent = false;
+    public:
+    Hungry(StatusBlock *parent);
+    Hungry(GameObject *target);
+};
+
+
+
+
+#endif
